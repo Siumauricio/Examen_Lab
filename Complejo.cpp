92815d491801de9d82c7e6bd71c99580f6487783
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-08-16 14:55:19 -0600

    Create Complejo.cpp

---
+++ Complejo.cpp
@@ -0,0 +1,63 @@
+#pragma once
+#include "Complejo.h"
+#include <iostream>
+using namespace std;
+
+Complejo::Complejo(double parteReal, double parteimaginaria):real(parteReal),imaginaria(parteimaginaria) {
+
+
+}
+
+Complejo Complejo::operator+(const Complejo& operando2)const {
+
+	return Complejo(real + operando2.real, imaginaria + operando2.imaginaria);
+
+}
+
+Complejo Complejo::operator-(const Complejo& operando2) const {
+
+	return Complejo(real - operando2.real, imaginaria - operando2.imaginaria);
+}
+
+Complejo Complejo::operator*(const Complejo& operando2)const {
+
+	return Complejo(real * operando2.real, imaginaria* operando2.imaginaria);
+}
+
+void Complejo::imprimir() const  {
+	cout << '(' << real << ", " << imaginaria << ')';
+} 
+
+bool Complejo::operator==(const Complejo& other)
+{
+	Complejo var;
+	if (var.real==other.real&&var.imaginaria==other.imaginaria)
+	{
+		return true;
+	}
+	return false;
+}
+bool Complejo::operator!=(const Complejo& other) {
+	Complejo var;
+	if (var.real!=other.real&& var.imaginaria==other.imaginaria)
+	{
+		return true;
+	}
+	return false;
+}
+
+std::ostream& operator<<(std::ostream& out, const Complejo& arr)
+{
+	out << "("<<arr.real<<")"<<"("<<arr.imaginaria<<")";
+
+	return out;
+}
+
+istream& operator >> (istream& in, Complejo& c)
+{
+	cout << "Ingresa la parte real: ";
+	in >> c.real;
+	cout << "Ingresa la parte imaginaria: ";
+	in >> c.imaginaria;
+	return in;
+}
